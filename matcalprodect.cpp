a67dab1922818bb5ec2f5915dbfbc2c974460dab
jameswsg2214 <36299263+jameswsg2214@users.noreply.github.com>
2018-03-04 17:53:12 +0530

    Create matcalprodect.cpp

---
+++ matcalprodect.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+class Feb{
+public:
+    int a[3][3],i,j,n,m,p,sum=1,sum1=1;
+    int get()
+    {
+cin>>n;
+if(n<=1000){
+   for(i=0;i<n;i++)
+   {
+       for(j=0;j<n;j++)
+       {
+           cin>>a[i][j];
+           }
+                      }
+}
+return 0;
+    }
+    int calc()
+    {
+
+   for(i=0;i<n;i++)
+   {
+       for(j=0;j<n;j++)
+       {
+            if(i==j)
+            sum=sum*a[i][j];
+       }
+   }
+
+   for(i=0,j=n-1;i<n,j>=0;i++,j--)
+   {
+           sum1=sum1*a[i][j];
+
+   }
+p=sum+sum1;
+cout<<p;
+return 0;
+    }
+};
+int main()
+{
+    Feb s;
+    s.get();
+    s.calc();
+    return 0;
+}
