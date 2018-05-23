532e116338700651743645c956e8e31018c9a570
jameswsg2214 <36299263+jameswsg2214@users.noreply.github.com>
2018-05-23 12:30:36 +0530

    Create checkpowervalueofgiven.cpp

---
+++ checkpowervalueofgiven.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include<cmath>
+#include<bits/stdc++.h>
+using namespace std;
+class sample
+{
+	public:
+    int i,k,n,c,count=0;
+	int get()
+{
+cin>>n>>c;
+}
+int calc(){
+	for(i=1;i<n;i++){
+		k=pow(c,i);
+		if(n==k){
+		count++;
+		i=n;
+		}
+	}
+	if(count>0)
+	cout<<"yes";
+	else
+	cout<<"no";
+}
+
+};
+
+int main()
+
+{
+
+   sample s;
+
+   s.get();
+
+   s.calc();
+
+   return 0;
+
+}
